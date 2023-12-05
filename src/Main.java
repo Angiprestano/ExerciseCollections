b0f8205fcfdab744455505f0d61ea602922e4038
Angela Prestano <angiprestano99@gmail.com>
2023-12-05 15:48:56 +0100

    Fine primo esercizio

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,15 @@ public class Main {
         Months Dicembre = new Months(31 , "Dicembre");
         Months Gennaio = new Months(31, "Gennaio");
         Months Febbraio = new Months(29, "Febbraio");
+
+
        Set<Months> monthsSet = new HashSet<>();
        monthsSet.add(Dicembre);
        monthsSet.add(Gennaio);
        monthsSet.add(Febbraio);
        System.out.println("Mese:" + " " + Dicembre);
-       System.out.println("Il numero delle parole è:" + " " + Dicembre.getWords() + " " + Dicembre.getWords());
-       System.out.println();
+       System.out.println("Parole duplicate:" + " " + Dicembre.getWords() + " " + Dicembre.getWords() + " " + Dicembre.getWords());
+       System.out.println("Numero di parole: " + monthsSet.size());
+       System.out.println("Elenco delle parole:" + " " + Dicembre.getWords() + " " + Gennaio.getWords() + " " + Febbraio.getWords());
     }
 }
